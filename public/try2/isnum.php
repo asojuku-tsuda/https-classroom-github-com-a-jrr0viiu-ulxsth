ab4d53600bfef78a27fc49d5c76d2117fe2a6804
yotu <114195789+ulxsth@users.noreply.github.com>
2025-04-15 01:06:35 +0000

    fix: 入力値のバリデーションを追加

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,9 +9,12 @@
     <div class="login-box">
       <h2>
 <?php
+$data = filter_input(INPUT_GET, "indata");
+if(!mb_ereg("^[0-9]+$", $data)) {
+  die("Error: 整数値ではない値が入力されています");
+}
 
-
-echo "入力された数字は： " . $_GET['indata'];
+echo "入力された数字は： " . $data;
 ?>
     </h2>
     </div>
